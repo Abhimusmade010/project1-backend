ded341474a63c66c9b5d6bfd3a126fc9ad841d7c
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-01-10 20:31:11 +0530

    modified index

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const path = require("path");
 const userRouter = require("./routes/userRoutes");
 const adminRouter = require("./routes/adminRoutes"); 
 
-
-
 const app = express();
 
 const allowedOrigins =
@@ -22,8 +20,6 @@ app.use(cors({
   credentials: true,
 }));
 
-
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -35,11 +31,9 @@ app.use(session({
     secure: process.env.NODE_ENV === 'production',          // Use secure cookies in production
     httpOnly: true,                                         // Prevent XSS attacks
     maxAge: 24 * 60 * 60 * 1000,                            // 24 hours session timeout
-    sameSite: 'strict'                                      // Prevent CSRF attacks
+    sameSite: none                                          // Prevent CSRF attacks
   }
 }))
-
-
 
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
