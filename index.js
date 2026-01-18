415319fb6630ad78906b456bc8f6f9f9faaed991
Abhishek Musmade <164621214+Abhimusmade010@users.noreply.github.com>
2026-01-18 09:42:18 +0530

    Update CORS configuration to use environment variable

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL,
-    origin: "http://localhost:3002",
+    origin: process.env.FRONTEND_URL,
+    // origin: "http://localhost:3002",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
